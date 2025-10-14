75a6c94db211243494cd36bf4f97e1b985390cec
Centhor <vincenthizon266@gmail.com>
2025-10-14 12:12:04 +0800

    git init

---
+++ src/main/java/com/oslab/App.java
@@ -0,0 +1,14 @@
+package com.oslab;
+
+public class App {
+    public static int arr[] = {1,2,3,4,5};
+    public static int head = 2;
+    public static int diskSize = 150;
+
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+        for(int a:arr){
+            System.out.println(a);
+        }
+    }
+}
